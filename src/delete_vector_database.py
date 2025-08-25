7a0c292b3307f4b0f0cc43d48af6741b10855d3e
MelvinAnthony <152087640+MelvinAnthony@users.noreply.github.com>
2025-08-25 15:53:33 +0530

    Refactor delete_database to support Pinecone

--- src/delete_vector_database.py
+++ src/delete_vector_database.py
@@ -1,38 +1,48 @@
-import weaviate
-from utils import setup_logger
-from dotenv import load_dotenv
 import os
+import logging
+from dotenv import load_dotenv
+import pinecone
 
+# Load environment variables
 load_dotenv()
+
+# Setup logger
+def setup_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    if not logger.hasHandlers():
+        logger.addHandler(handler)
+    return logger
 
 logger = setup_logger(__name__)
 
-
-def delete_database(index_name: str, vector_store: str = "weaviate"):
+def delete_database(index_name: str, vector_store: str = "pinecone"):
     """
-    Deletes a class from a vector database. Currently only supports weaviate.
+    Deletes an index from Pinecone.
 
     Parameters:
     ---
-    - weaviate_client (weaviate.Client): An instance of the Weaviate client, connected to your Weaviate instance.
-    - index_name (str): The name of the class in the Weaviate schema you wish to delete.
-    - vector_store (str, optional): The name of the vector store in which you wish to delete the index.
-    Currently only supports weaviate.
+    - index_name (str): The name of the index in Pinecone you wish to delete.
+    - vector_store (str, optional): Currently only supports Pinecone.
     """
-    if vector_store == "weaviate":
+    if vector_store == "pinecone":
         try:
-            WEAVIATE_HOST = os.environ.get("WEAVIATE_HOST")
-            client = weaviate.Client(url=WEAVIATE_HOST)
-            client.schema.delete_class(index_name)
-            logger.info(
-                f"Successfully deleted the vector index '{index_name}' from {vector_store}"
-            )
+            # Initialize Pinecone client
+            PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+            pinecone.init(api_key=PINECONE_API_KEY)
+
+            # Delete the index
+            pinecone.delete_index(index_name)
+
+            logger.info(f"Successfully deleted the index '{index_name}' from Pinecone")
+
         except Exception as e:
-            logger.error(
-                f"Error deleting the vector index '{index_name}' from {vector_store}"
-            )
+            logger.error(f"Error deleting the index '{index_name}' from Pinecone: {str(e)}")
 
 
 if __name__ == "__main__":
-    vector_index_to_delete = "MedicineIndex"
+    vector_index_to_delete = "CricketIndex"  # change to your index name
     delete_database(vector_index_to_delete)
